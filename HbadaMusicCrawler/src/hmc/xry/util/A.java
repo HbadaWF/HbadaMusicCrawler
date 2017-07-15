737de735a77150c16290588e216086b27bad49b2
XIURongYao <1533651510@qq.com>
2017-07-15 22:10:56 +0800

    修改了.gitignore文件，让jar文件也可以上传,这一次真正把jar上传到了github

---
+++ HbadaMusicCrawler/src/hmc/xry/util/A.java
@@ -0,0 +1,16 @@
+package hmc.xry.util;
+/**
+ *<p>Copyright 2017 OSFF</p>
+ *<p></p>
+ *<p>类描述  ：[这个类是：]</p>
+ *<p>创建人  ：[李玉增] </p>
+ *<p>创建时间：[2017年7月15日下午12:45:50]</p>
+ *<p>修改人  ：[李玉增] 描述：[]</p>
+ */
+public class A {
+	
+	public static void main(String[] args) {
+		System.out.println("say hello!");
+	}
+
+}
